c0d7bc5e984cc2f25661ebb39cf2ccedc82a3a92
Vincent John <vincejohn987@gmail.com>
2022-07-12 08:07:47 -0700

    My buffer

---
+++ wBuffer.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _write_buffer - function that initializes and writes a buffer
+ * @buffer: buffer size
+ */
+
+void _write_buffer(char *buffer, int *index)
+{
+	int i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		buffer[i] 0;
+	}
+	buffer[i] = 0;
+}
